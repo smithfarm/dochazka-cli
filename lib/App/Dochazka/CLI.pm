4351b71ff0e187d2ea08ae1da8403a7cd57c32f5
Nathan Cutler <ncutler@suse.cz>
2015-07-23 18:49:18 +0200

    0.201
    Signed-off-by: Nathan Cutler <ncutler@suse.cz>

--- lib/App/Dochazka/CLI.pm
+++ lib/App/Dochazka/CLI.pm
@@ -53,11 +53,11 @@ App::Dochazka::CLI - Dochazka command line client
 
 =head1 VERSION
 
-Version 0.200
+Version 0.201
 
 =cut
 
-our $VERSION = '0.200';
+our $VERSION = '0.201';
 
 
 
--- lib/App/Dochazka/CLI/Guide.pm
+++ lib/App/Dochazka/CLI/Guide.pm
@@ -42,11 +42,11 @@ App::Dochazka::CLI::Guide - Dochazka CLI Guide
 
 =head1 VERSION
 
-Version 0.200
+Version 0.201
 
 =cut
 
-our $VERSION = '0.200';
+our $VERSION = '0.201';
 
 
 
