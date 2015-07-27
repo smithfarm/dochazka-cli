c1ad9866d584b86ddc08eae833c48b671ac76f9d
Nathan Cutler <ncutler@suse.cz>
2015-07-27 21:33:13 +0200

    Util.pm: make CRIT log message more reasonable
    by writing it conditionally instead of unconditionally.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.cz>

--- lib/App/Dochazka/CLI/Util.pm
+++ lib/App/Dochazka/CLI/Util.pm
@@ -496,8 +496,10 @@ REST calls are cheap, so look up C<< $current_emp >> again just to make sure.
 
 sub refresh_current_emp {
     my $status = send_req( 'GET', 'employee/eid/' . $current_emp->eid );
-    $log->crit( "Problem with data integrity (current employee)" );
-    return $status unless $status->ok;
+    if ( $status->not_ok ) {
+        $log->crit( "Problem with data integrity (current employee)" );
+        return $status;
+    }
     $current_emp = App::Dochazka::Common::Model::Employee->spawn( %{ $status->payload } );
     return $status;
 }
