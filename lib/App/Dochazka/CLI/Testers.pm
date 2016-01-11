552904da579be04190972fd20feec01b50402002
Nathan Cutler <ncutler@suse.com>
2016-01-12 00:40:12 +0100

    Testers.pm: first test is to meet the prerequisites
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/Dochazka/CLI/Testers.pm
+++ lib/App/Dochazka/CLI/Testers.pm
@@ -47,6 +47,58 @@ App::Dochazka::CLI::Testers - Documentation for volunteer testers
 
 
 
+=head1 PREREQUISITES
+
+Before you start, you will need to complete the following steps.
+
+=head2 Install Docker
+
+For testing purposes, you can use the Dockerized REST server. For this, you
+will need to have Docker installed and running:
+
+    zypper in docker
+    systemctl start docker.service
+
+=head2 Get and run test drive script
+
+The REST server Docker image depends on the official PostgreSQL image and
+must be run with certain parameters. A script is provided to make this
+easy. First, download the script:
+
+    wget https://raw.githubusercontent.com/smithfarm/dochazka-rest/master/test-drive.sh
+
+Then run it:
+
+    sh test-drive.sh
+
+When the script completes, you should be able to access the REST server at
+L<http://localhost:5000>. (It will display a login prompt - you can ignore it.)
+
+=head2 Add zypper repo and install CLI
+
+Next, add the C<home:smithfarm> zypper repo for your operating system:
+L<http://software.opensuse.org/download.html?project=home%3Asmithfarm&package=perl-App-Dochazka-CLI>.
+
+And, finally, install the CLI:
+
+    zypper install perl-App-Dochazka-CLI
+
+=head2 Verify success
+
+Provided you have successfully completed all of the above steps, you should
+be able to start the CLI and login as "root" with password "immutable":
+
+    $ dochazka-cli -u root -p immutable
+    Loading configuration files from
+    /usr/lib/perl5/vendor_perl/5.18.2/auto/share/dist/App-Dochazka-CLI
+    Cookie jar: /root/.cookies.txt
+    URI base http://localhost:5000 set from site configuration
+    Authenticating to server at http://localhost:5000 as user root
+    Server is alive
+    Dochazka(2016-01-12) root ADMIN>
+
+Congratulations! You have passed the first test.
+
 =cut
 
 1;
