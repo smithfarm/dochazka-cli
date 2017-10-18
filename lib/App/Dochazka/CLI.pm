a6caa64bae2b939093ee987882f0bb298f6d1c91
Nathan Cutler <ncutler@suse.com>
2017-10-18 22:54:30 +0200

    0.239
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- lib/App/Dochazka/CLI.pm
+++ lib/App/Dochazka/CLI.pm
@@ -53,11 +53,11 @@ App::Dochazka::CLI - Dochazka command line client
 
 =head1 VERSION
 
-Version 0.238
+Version 0.239
 
 =cut
 
-our $VERSION = '0.238';
+our $VERSION = '0.239';
 
 
 
--- lib/App/Dochazka/CLI/Guide.pm
+++ lib/App/Dochazka/CLI/Guide.pm
@@ -42,11 +42,11 @@ App::Dochazka::CLI::Guide - Dochazka CLI Guide
 
 =head1 VERSION
 
-Version 0.238
+Version 0.239
 
 =cut
 
-our $VERSION = '0.238';
+our $VERSION = '0.239';
 
 
 
